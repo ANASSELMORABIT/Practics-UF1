0594938d0001f3a53b05e2ac1c4409269df8bc56
Anass <anasselmorabit797@gmail.com>
2023-11-10 11:57:54 +0100

    Programmacion de C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int numA,numB,Max;
+        Console.WriteLine("ingresar el primero numero:");
+        numA=Convert.ToInt32(System.Console.ReadLine());
+        Console.WriteLine("ingresar el segundo numero:");
+        numB=Convert.ToInt32(System.Console.ReadLine());
+        if(numA > numB) {
+            Console.WriteLine("el numero mayro es: "+numA);
+        }
+        else if (numA < numB){
+            Console.WriteLine("el numero mayro es: "+numB);
+        }
+        else{
+            Console.WriteLine("ambos numeros son iguales");
+        }
+    }
+}
